81df9e590f1f8790059030706108addbe8e90be4
trevorlamoglia <trevorlamoglia21@gmail.com>
2021-01-31 22:11:37 -0700

    Preston webpage update

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,8 @@
+let currentDate = new Date();
+let Year = currentDate.getFullYear();
+document.getElementById("year").textContent = Year;
+
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
