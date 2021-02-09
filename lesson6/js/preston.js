37dfee63ce5d4a1da7bf79f08cab4056f75c142a
trevorlamoglia <trevorlamoglia21@gmail.com>
2021-02-08 17:24:48 -0700

    created lesson 6 adn lesson 4 update

---
+++ lesson6/js/preston.js
@@ -0,0 +1,46 @@
+// Footer year js
+let currentDate = new Date();
+let Year = currentDate.getFullYear();
+document.getElementById("year").textContent = Year;
+
+// Toggle js
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+// Current date js
+const dayNames = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const dateToday = new Date();
+const dayName = dayNames[dateToday.getDay()];
+const monthName = months[dateToday.getMonth()];
+const year = dateToday.getFullYear();
+const presentDate = dayName + "," + " " + dateToday.getDate() + " " + monthName + " " + year;
+document.getElementById("presentDate").textContent = presentDate;
+
+// Show date message
+const today = new Date();
+const dayNumber = today.getDay();
+
+const element = document.getElementById("message");
+if (dayNumber == 6) {
+    element.classList.add("showme");
+}
+else {
+    element.classList.add("hideme");
+}
