8cdd316e80d52e0b518a98f9d10b5b5545df5f40
trevorlamoglia <trevorlamoglia21@gmail.com>
2021-02-12 16:17:26 -0700

    Lesson 6 complete

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,18 @@
+const tempNumber = parseFloat(document.getElementById('temp').textContent);
+// console.log(tempNumber)
+
+const speedNumber = parseFloat(document.getElementById('speed').textContent);
+// console.log(speedNumber)
+
+let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16))
++ (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+// console.log(windchill);
+windchill = Math.round(windchill);
+// console.log(windchill);
+
+if (tempNumber <= 50 && speedNumber > 3) {
+    document.getElementById('chill').textContent = "Wind Chill is "+windchill+"\xB0F";
+}
+else {
+    document.getElementById('chill').textContent = "No Wind Chill Today"
+}
