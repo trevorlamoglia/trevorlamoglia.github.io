891afb24de6a1340acbb35fff701a4cc76439b3e
Trevor Lamoglia <trevorlamoglia21@gmail.com>
2021-03-18 22:43:17 -0600

    lesson11 update

--- lesson11/js/fishhaven.js
+++ lesson11/js/fishhaven.js
@@ -46,3 +46,14 @@ fetch(forecastURL)
       }
 
     });
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json'
+fetch(requestURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        document.getElementById('event1').textContent = jsObject.towns[2].events[0];
+        document.getElementById('event2').textContent = jsObject.towns[2].events[1];
+        document.getElementById('event3').textContent = jsObject.towns[2].events[2];
+    });
