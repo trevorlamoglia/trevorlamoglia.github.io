46e44717bf7e2a64cba1f31dc9b1d6d81c3016cd
Trevor Lamoglia <trevorlamoglia21@gmail.com>
2021-04-01 19:20:32 -0600

    trial

--- final/js/events.js
+++ final/js/events.js
@@ -4,7 +4,7 @@ fetch(requestURL)
     .then((jsObject) => {
         console.log(jsObject);
 
-        // document.getElementById('event1').textContent = jsObject.towns[6].events[0];
-        // document.getElementById('event2').textContent = jsObject.towns[6].events[1];
-        // document.getElementById('event3').textContent = jsObject.towns[6].events[2];
+        document.getElementById('event1').textContent = jsObject.towns[6].events[0];
+        document.getElementById('event2').textContent = jsObject.towns[6].events[1];
+        document.getElementById('event3').textContent = jsObject.towns[6].events[2];
     });
