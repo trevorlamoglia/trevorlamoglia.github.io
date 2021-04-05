9b6b7c88058c6e23cc6e5209ebaea96e1852392c
Trevor Lamoglia <trevorlamoglia21@gmail.com>
2021-04-04 22:23:37 -0600

    update

--- final/js/directory.js
+++ final/js/directory.js
@@ -4,4 +4,24 @@ fetch(requestURL)
     .then((response) => response.json())
     .then((jsonObject) => {
         console.table(jsonObject);
+        const companies = jsonObject['companies'];
+
+        for (let i = 0; i < companies.length; i++) {
+            let card = document.createElement('div');
+            let h2 = document.createElement('h2');
+            let contact = document.createElement('p');
+            let image = document.createElement('img');
+
+            h2.textContent = companies[i].name;
+            contact.textContent = companies[i].contact;
+            image.setAttribute('src', 'images/' + companies[i].photo);
+            image.setAttribute('alt', companies[i].name + ' logo');
+
+            card.appendChild(image);
+            card.appendChild(h2);
+            card.appendChild(contact);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+
     })
