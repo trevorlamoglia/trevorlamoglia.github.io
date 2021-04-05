27a84c3ae611e12c56763cbbb3c15fa077f53d69
Trevor Lamoglia <trevorlamoglia21@gmail.com>
2021-04-04 22:36:37 -0600

    update

--- final/js/directory.js
+++ final/js/directory.js
@@ -9,16 +9,25 @@ fetch(requestURL)
         for (let i = 0; i < companies.length; i++) {
             let card = document.createElement('div');
             let h2 = document.createElement('h2');
+            let industry = document.createElement('p');
+            let hq = document.createElement('p');
+            let founded = document.createElement('p');
             let contact = document.createElement('p');
             let image = document.createElement('img');
 
             h2.textContent = companies[i].name;
+            industry.textContent = companies[i].industry;
+            hq.textContent = companies[i].headquarters;
+            founded.textContent = companies[i].founded;
             contact.textContent = companies[i].contact;
             image.setAttribute('src', 'images/' + companies[i].photo);
             image.setAttribute('alt', companies[i].name + ' logo');
 
             card.appendChild(image);
             card.appendChild(h2);
+            card.appendChild(industry);
+            card.appendChild(hq);
+            card.appendChild(founded);
             card.appendChild(contact);
 
             document.querySelector('div.cards').appendChild(card);
